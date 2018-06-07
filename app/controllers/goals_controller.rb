a6070214f172bcd5793546cbe29c24b9e3464ff2
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:53 +0000

    Only goal user can access show page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,5 @@
 class GoalsController < ApplicationController
-  before_action :current_user_must_be_goal_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_goal_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_goal_user
     goal = Goal.find(params[:id])
