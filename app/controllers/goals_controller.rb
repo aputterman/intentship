848bf0d953260133a2b8ba758d2a6e9d8b59a118
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:53 +0000

    Only goal user can modify goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,14 @@
 class GoalsController < ApplicationController
+  before_action :current_user_must_be_goal_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_goal_user
+    goal = Goal.find(params[:id])
+
+    unless current_user == goal.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @goals = Goal.all
 
