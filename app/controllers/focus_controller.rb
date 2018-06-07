7ba82ba77d4dd8ee3caf05258c3ac82879986db9
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:53 +0000

    Only focu user can modify focu

--- app/controllers/focus_controller.rb
+++ app/controllers/focus_controller.rb
@@ -1,4 +1,14 @@
 class FocusController < ApplicationController
+  before_action :current_user_must_be_focu_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_focu_user
+    focu = Focu.find(params[:id])
+
+    unless current_user == focu.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @focus = Focu.all
 
