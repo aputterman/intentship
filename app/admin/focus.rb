53338a0dcaef18f68c1c9e814865b298bf5425a1
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:33 +0000

    Whitelist columns in Focu for ActiveAdmin

--- app/admin/focus.rb
+++ app/admin/focus.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Focu do
+
+ permit_params :name, :goals_id, :mindsets_id, :description, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
