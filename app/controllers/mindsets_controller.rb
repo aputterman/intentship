42009a3b293d3e4c4592b373746247d48986e67c
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:53 +0000

    Only mindset user can access show page

--- app/controllers/mindsets_controller.rb
+++ app/controllers/mindsets_controller.rb
@@ -1,5 +1,5 @@
 class MindsetsController < ApplicationController
-  before_action :current_user_must_be_mindset_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_mindset_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_mindset_user
     mindset = Mindset.find(params[:id])
