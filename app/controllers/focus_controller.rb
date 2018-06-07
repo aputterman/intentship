317c538f68e81a9ed73d8dd18baa6007299f37a8
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:53 +0000

    Only focu user can access show page

--- app/controllers/focus_controller.rb
+++ app/controllers/focus_controller.rb
@@ -1,5 +1,5 @@
 class FocusController < ApplicationController
-  before_action :current_user_must_be_focu_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_focu_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_focu_user
     focu = Focu.find(params[:id])
