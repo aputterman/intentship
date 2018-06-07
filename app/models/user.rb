7d385379d31975e4c63df7788a98222dea02be5f
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:25:39 +0000

    Validate presence of users.name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :name, :presence => { :message => "whoa there - you can't NOT fill this out" }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
