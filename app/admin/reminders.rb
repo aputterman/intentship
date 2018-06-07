dbff91328d454eb06f3773696bf371e742f56422
Raghu Betina <raghu@firstdraft.com>
2018-06-07 14:27:48 +0000

    Whitelist columns in Reminder for ActiveAdmin

--- app/admin/reminders.rb
+++ app/admin/reminders.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Reminder do
+
+ permit_params :focus_id, :mindsets_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
